fb82a1f5bc55784287190c78a1085e15496fcde1
SOFe <sofe2038@gmail.com>
2019-03-01 23:54:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CubePump Client
+
---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+/*
+ * CubePump
+ *
+ * Copyright (C) 2019 SOFe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+extern crate cfg_if;
+extern crate wasm_bindgen;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+
+mod utils;
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn init_cube_pump() {
+    alert("Hello again, cube-pump!");
+}
